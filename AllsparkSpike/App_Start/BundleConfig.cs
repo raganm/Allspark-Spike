ab9245d385494e0886d8bd0c721bd9a2cc7547a0
Ragan McGill <ragan.mcgill@comparethemarket.com>
2015-02-07 11:54:20 +0000

    .

--- AllsparkSpike/App_Start/BundleConfig.cs
+++ AllsparkSpike/App_Start/BundleConfig.cs
@@ -27,7 +27,8 @@ namespace AllsparkSpike
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/toolbox2.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
